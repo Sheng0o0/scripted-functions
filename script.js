6a3626665abc3ef1acdf8ad51bca9258af09e156
Davis Barber <dbarber@squarespace.com>
2016-01-14 09:59:36 -0500

    Second commit

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 var sum = 2 + 2;
-sum = sum + 5;
-sum = sum - 2;
-sum = sum + 6;
 
 if (sum > 10) {
-  alert('Is greater than 10.');
+  document.write('Is greater than 10');
 } else {
-  alert('Is not greater than 10.');
+  document.write('Is not greater than 10.');
 }
